c28bcaca7d4d6b1e948eeef37a5c473c62429f6e
Peter Pham <phamhere@gmail.com>
2018-10-05 10:42:27 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Avengers: by Peter Pham.
           </p>
           <a
             className="App-link"
